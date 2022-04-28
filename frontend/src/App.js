b6aa3b99ebabb0ab5240f754a117abb66ee3d82e
Braian Bilicic <braian.bilicic@gmail.com>
2022-04-28 11:03:21 -0300

    Create-react-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+      <main>LIST PRODUCTS</main>
+    </div>
+  );
+}
+
+export default App;
